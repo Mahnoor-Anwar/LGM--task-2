8f45fe8358f53ec968955c9ad5b0866e828f5c6a
Mahnoor-Anwar <mahnoordev@gmail.com>
2021-09-30 01:10:49 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
 
-function App() {
+const App = () => {
+  const [users, setUser] = useState([]);
+
+  const loadUser = async () => {
+    const response = await fetch("https://api.github.com/users");
+    const loadresponse = await response.json();
+    setUser(loadresponse);
+    // document.write(setUser);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LGM-task-2</h1>
+      <button onClick={loadUser}>Get User</button>
+      <h2>Users:</h2>
+      <ul>
+        {users.map(({ id, login }) => (
+          <li key={id}>Name: {login}</li>
+        ))}
+        {/* {user.map((email) => {
+          return <li>{email}</li>;
+        })}; */}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
